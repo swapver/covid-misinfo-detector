5e80d6523c22d3029fcca4afb399e22a59e25493
swapver <58562361+swapver@users.noreply.github.com>
2021-05-02 10:33:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, url_for, Markup, jsonify
+import pickle
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+pickle_in = open('model_fakenews.pickle','rb')
+pac = pickle.load(pickle_in)
+tfid = open('tfid.pickle','rb')
+tfidf_vectorizer = pickle.load(tfid)
+
+@app.route('/')
+def home():
+ 	return render_template("index.html")
+
+@app.route('/newscheck')
+def newscheck():	
+	abc = request.args.get('news')	
+	input_data = [abc.rstrip()]
+	# transforming input
+	tfidf_test = tfidf_vectorizer.transform(input_data)
+	# predicting the input
+	y_pred = pac.predict(tfidf_test)
+	return jsonify(result = y_pred[0])
+
+
+if __name__=='__main__':
+    app.run()
